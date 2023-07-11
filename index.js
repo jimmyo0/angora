1d54f84e2f1eaff1595f1b1463de91f86cb03f41
Ahmed gamal <ag555293@gmail.com>
2023-07-11 21:54:29 +0300

    first comit

---
+++ index.js
@@ -0,0 +1,299 @@
+ 1
+// var input = Number( window.prompt("type here"))
+// console.log (input)
+
+ 2
+//  var input = Number( window.prompt("type here"))
+// if(input % 3 == 0) 
+// {
+//     console.log("yes");
+// }else if(input % 4 == 0)
+// {
+//     console.log("yes");
+// }
+// else{
+//     console.log("no")
+// }
+
+
+ 3
+// var input1 = Number( window.prompt("type here"))
+// var input2 = Number( window.prompt("type here"))
+// if (input1 > input2) {
+//     console.log(input1);
+// }else if(input1 < input2){
+//     console.log(input2);
+// }
+
+
+ 4
+// var input = Number( window.prompt("type here"))
+// if (input >= 0) {
+//     console.log("positive");
+// } else {
+//     console.log("negative");
+// }
+
+ 5
+// var input1 = Number( window.prompt("type here"))
+// var input2 = Number( window.prompt("type here"))
+// var input3 = Number( window.prompt("type here"))
+// if (input1 > input2 && input1 > input3) {
+//     console.log("max element =" , input1)
+// }else if (input2 > input1 && input2 > input3 ) {
+//     console.log("max element =" , input2);
+// }else if (input3 > input1 && input3 > input2) {
+//     console.log("max element =" , input3);
+// }
+
+// if (input1 < input2 && input1 < input3) {
+//     console.log("min element =" , input1)
+// }else if (input2 < input1 && input2 < input3 ) {
+//     console.log("min element =" , input2);
+// }else if (input3 < input1 && input3 < input2) {
+//     console.log("min element =" , input3);
+// }
+
+ 6
+// var input = Number( window.prompt("type here"))
+//  if(input % 2 == 0) 
+//  {
+//      console.log("even");
+//  }else
+//  {
+//      console.log("odd");
+//  }
+
+ 7
+//  var input =  window.prompt("type here")
+// if (input == "a" || input == "A" || input == "e" || input == "E" || input == "i" || input == "I" || input == "o" || input == "O" || input == "u" || input == "U") {
+//     console.log("vowel")
+// }else{
+//     console.log("consonant");
+// }
+
+ 8
+//  var input = Number( window.prompt("type here"))
+//  for(i=1 ;i<=input ;i++ ){
+//     console.log(i);
+//  }
+
+ 9
+//  var input = Number( window.prompt("type here"))
+//  for(i=input; i <= input * 12 ;i+=input )
+//  {
+//     console.log(i)
+//  }
+
+ 10
+// var input = Number( window.prompt("type here"))
+// if (input % 2 == 0) {
+//     for(i=2;i<=input;i+=2){
+//         console.log(i);
+//     }
+// } else {
+//     var num = input--
+//     for(i=2;i<=num;i+=2){
+//         console.log(i);
+//     }
+// }
+
+ 11
+//  var input1 = Number( window.prompt("type here"))
+//  var input2 = Number( window.prompt("type here"))
+//  console.log(input1 ** input2);
+
+ 12
+// var input1 = Number( window.prompt("subject 1"))
+// var input2 = Number( window.prompt("subject 2"))
+// var input3 = Number( window.prompt("subject 3"))
+// var input4 = Number( window.prompt("subject 4"))
+// var input5 = Number( window.prompt("subject 5"))
+// var total =  input1+input2+input3+input4+input5
+// console.log("total marks ="+ total);
+// console.log("Average marks ="+ Number (total / 5 ));
+// console.log("Percentage =" + Number (total / 500 *100));
+
+ 13
+//  var input = Number( window.prompt("type month number :"))
+// if (input == 1 || input == 3 || input == 5 || input == 7 || input == 8 || input == 10 || input == 12 ) {
+//     console.log(31);
+// }else if (input == 2 ){
+//     console.log(28);
+// }else if(input == 4 || input == 6 || input == 9 || input == 11 ){
+//     console.log(30);
+// }
+
+ 14
+//  var input1 = Number( window.prompt("Physics"))
+//  var input2 = Number( window.prompt("Chemistry"))
+//  var input3 = Number( window.prompt("Biology"))
+//  var input4 = Number( window.prompt("mathematics"))
+//  var input5 = Number( window.prompt("computer"))
+//  var total =  input1+input2+input3+input4+input5;
+//  var Percent = + Number (total / 500 *100);
+// console.log( "percentage"+ Percent +"%");
+//  if (Percent >= 90) {
+//     console.log("Grad A");
+//  } else if (Percent >= 80) {
+//     console.log("Grad B");
+//  }
+ 
+//  else if (Percent >= 70) {
+//     console.log("Grad C");
+//  } 
+ 
+//  else if (Percent >= 60) {
+//     console.log("Grad D");
+//  }
+ 
+//  else if (Percent >= 40) {
+//     console.log("Grad E");
+//  } 
+ 
+//  else if (Percent < 40) {
+//     console.log("Grad F");
+//  }
+ 15
+// var input =  window.prompt("type month number :")
+// switch(input)
+// {
+// case"1":
+//         console.log(31);
+//         break
+// case"3":
+//         console.log(31);
+//         break
+// case"5":
+//         console.log(31);
+//         break
+// case"7":
+//         console.log(31);
+//         break
+// case"8":
+//         console.log(31);
+//         break
+// case"10":
+//         console.log(31);
+//         break
+// case"12":
+//         console.log(31);
+//         break
+// case"2":
+//         console.log(28);
+//         break
+// case"4":
+//     console.log(30);
+//     break
+// case"6":
+//     console.log(30);
+//     break
+// case"9":
+//     console.log(30);
+//     break
+// case"11":
+//     console.log(30);
+//     break
+// }
+
+ 16
+// var input =  window.prompt("type here")
+// switch (input) {
+//     case "a":
+//         console.log("vowel")
+//         break;
+//     case "A":
+//         console.log("vowel")
+//         break;
+//     case "e":
+//         console.log("vowel")
+//         break;
+//     case "E":
+//         console.log("vowel")
+//         break;
+//     case "i":
+//         console.log("vowel")
+//         break;
+//     case "I":
+//         console.log("vowel")
+//         break;
+//     case "o":
+//         console.log("vowel")
+//         break;
+//     case "O":
+//         console.log("vowel")
+//         break;
+//     case "u":
+//         console.log("vowel")
+//         break;
+//     case "U":
+//         console.log("vowel")
+//         break;
+//     default :
+//         console.log("consonant");
+//         break
+// }
+
+ 17
+// var input1 = Number( window.prompt("type here")) 
+// var input2 = Number( window.prompt("type here"))
+// var res =input1 - input2
+// switch (input1>input2) {
+//     case true :
+//         console.log(input1);
+//         break;
+
+//     default:
+//         console.log(input2);
+//         break;
+// }
+
+18
+// var input1 = Number( window.prompt("type here")) 
+// switch (input1 % 2 ==0) {
+//     case true:
+//         console.log("even")
+//         break;
+
+//     default:
+//         console.log("odd")
+//         break;
+// }
+
+19
+// var input1 = Number( window.prompt("type here")) 
+// switch (input1 > 0) {
+//     case true:
+//         console.log("positive")
+//         break;
+// }
+
+// switch (input1 < 0) {
+//     case true:
+//         console.log("negative")
+//         break;
+// }
+// switch (input1= 0) {
+//     case true:
+//         console.log("zero")
+//         break;
+// }
+
+20
+// var a = Number( window.prompt("type first number")) 
+// var o =  window.prompt("type operator") 
+// var b = Number( window.prompt("type last number")) 
+// switch (o) {
+//     case "+":
+//         console.log(a + b);
+//         break;
+//     case "-":
+//         console.log(a - b);
+//         break;
+//     case "*":
+//         console.log(a * b);
+//         break;
+//     case "/":
+//         console.log(a / b);
+//         break;
+// }
